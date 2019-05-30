2beb538c03d45656ad23e3112714f711d27369a3
Petr Barton <petrbarton2@gmail.com>
2019-05-30 22:35:36 +0200

    add match

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Match(models.Model):
+    player1 = models.CharField(max_length=200)
+    player2 = models.CharField(max_length=200)
+    date_played = models.DateTimeField()
+    result = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.player1} vs {self.player2}"
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from main.models import Match
 
-def homepage(requests):
-    return HttpResponse("hommmm")
+
+def homepage(request):
+    return render(request, template_name='main/home.html', context={'matches': Match.objects.all})
